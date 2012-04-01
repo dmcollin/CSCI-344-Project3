a9692e4fb5dfd88dd9f0e3c2df0b43c6d6046751
Danielle Collins <dmcollin@unca.edu>
2012-04-01 12:13:33 -0400

    edited awesome.js to add actual links

--- app/controllers/word/awesome.js
+++ app/controllers/word/awesome.js
@@ -5,13 +5,14 @@ var client = redis.createClient();
 exports.index = function(req, res) {
     client.zrevrangebyscore('awesome', '+inf', '-inf', 'withscores', function(error, results){
     var responseString = "";
+    var limit = 38; //will display 20, since the counts are part of the results
 
-      for (var i = 0 ; i < results.length ; i++) {
+      for (var i = 0 ; i < limit ; i++) { 
         if (i%2 == 0){
-          responseString += results[i];
+          responseString += "<li> <a href='>" + results[i] + "'>" + results[i] + "'</a>";
         }
        else if (i%2 == 1){
-          responseString += " - " + results[i] + "<br />";
+          responseString += " - " + results[i] + "</li><br />";
        }
       }
       res.render('word/awesome', {awesomeLinks:responseString});
