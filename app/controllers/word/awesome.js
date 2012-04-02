133c0e13dc3f022d08e23d31daf7adff892d1512
Danielle Collins <dmcollin@unca.edu>
2012-04-01 20:04:30 -0400

    edited awesome.js to not display undefined if less links than limit

--- app/controllers/word/awesome.js
+++ app/controllers/word/awesome.js
@@ -6,15 +6,27 @@ exports.index = function(req, res) {
     client.zrevrangebyscore('awesome', '+inf', '-inf', 'withscores', function(error, results){
     var responseString = "";
     var limit = 38; //will display 20, since the counts are part of the results
-
-      for (var i = 0 ; i < limit ; i++) { 
-        if (i%2 == 0){
-          responseString += "<li> <a href='>" + results[i] + "'>" + results[i] + "'</a>";
+      if (results.length > limit){
+        for (var i = 0 ; i < limit ; i++) { 
+          if (i%2 == 0){
+            responseString += "<li> <a href='>" + results[i] + "'>" + results[i] + "'</a>";
+          }
+         else if (i%2 == 1){
+            responseString += " - " + results[i] + "</li><br />";
+         }
         }
-       else if (i%2 == 1){
-          responseString += " - " + results[i] + "</li><br />";
-       }
       }
+      else {
+        for (var i = 0 ; i < results.length ; i++) { 
+          if (i%2 == 0){
+            responseString += "<li> <a href='>" + results[i] + "'>" + results[i] + "'</a>";
+          }
+         else if (i%2 == 1){
+            responseString += " - " + results[i] + "</li><br />";
+         }
+        }
+      }
+
       res.render('word/awesome', {awesomeLinks:responseString});
   }); 
 
