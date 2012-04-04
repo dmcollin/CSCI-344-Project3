8766b545fca81420fd023bf5e62062b37b631ff8
Danielle Collins <dmcollin@unca.edu>
2012-04-04 16:21:07 -0400

    final layout changes and minor code fixes

--- app/controllers/word/awesome.js
+++ app/controllers/word/awesome.js
@@ -10,52 +10,53 @@ exports.index = function(req, res) {
     var topLinkString = "";
     var topLinkNumTweets;
     var mostTracking = 20; //will display 9, since the scores are part of the results
-	var limit;
-	
-	//check amount of links in the results to display no more than amount assigned in mostTracking variable
-	if (results.length > mostTracking){
-		limit = mostTracking
-	}
-	else {
-		limit = results.length;
-	}
+var limit;
 
-	//pull top link to send via topAwesomeLink
-    topLinkString += "<a href='" + results[0] + "'>" + results[0] + "</a>"; 
+  //check amount of links in the results to display no more than amount assigned in mostTracking variable
+    if (results.length > mostTracking){
+       limit = mostTracking
+    }
+    else {
+        limit = results.length;
+    }
 
+  //pull top link to send via topAwesomeLink
+  if (results[0].length > 40){ //if string length is greater than, add "..."
+      topLinkString += "<a href='" + results[0] + "' target='blank'>" + results[0].substring(0.38) + "...</a>";
+  }
+  else {
+    topLinkString += "<a href='" + results[0] + "' target='blank'>" + results[0] + "</a>";
+  }
   //pull score from top link to send via topAwesomeLinkNumTweets
     topLinkNumTweets = results[1];
 
-	//pull remaining links and attach to responseString
+//pull remaining links and attach to responseString
     for (var i = 2 ; i < limit ; i++) {
-      		  if (i%2 == 0){
+       if (i%2 == 0){
               if (results[i].length > 40){
                 //if string length is greater than, add "..."
-      			     responseString += "<a href='" + results[i] + "'>" + results[i].substring(0,38)  + "...</a>";
+                responseString += "<a href='" + results[i] + "' target='blank'>" + results[i].substring(0,38) + "...</a>";
               }
               else {
-                responseString += "<a href='" + results[i] + "'>" + results[i].substring(0,38)  + "</a>";
+                responseString += "<a href='" + results[i] + "' target='blank'>" + results[i].substring(0,38) + "</a>";
                   
               }
 
              }
             
-      		  else if (i%2 == 1){
-              if (results[i] > 1 && results[i-1] != "null"){
-      			     responseString += " (Tweeted " + results[i] + " times!) <br />";
+       else if (i%2 == 1){
+              if (results[i] > 1){
+                responseString += " (Tweeted <font color='#000'>" + results[i] + "<font color='#3cb3b5'> times!) <br />";
                }
-              else if (results[i] = 1 && results[i-1] != "null"){
-                responseString += " (Tweeted just once.) <br />";
+              else if (results[i] = 1){
+                responseString += " (Tweeted <font color='#000'>just once<font color='#3cb3b5'>.) <br />";
               }
             }
      
     }
 
-	  //send responseString to awesomeLinks, topLinkString to topAwesomeLink, and topLinkNumTweets to be displayed via awesome.ejs
+//send responseString to awesomeLinks, topLinkString to topAwesomeLink, and topLinkNumTweets to be displayed via awesome.ejs
       res.render('word/awesome', {awesomeLinks:responseString, topAwesomeLink:topLinkString, topAwesomeLinkNumTweets:topLinkNumTweets});
   });
 
 };
-
-
-
